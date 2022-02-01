eb25ee78f7e0508e7552e8e944f625b1b7504921
Carlos Roberto Faria da Silva <ocarlosroberto@gmail.com>
2022-02-01 09:40:02 -0300

    feat: Create Dashboard Component

--- ClientApp/src/app/components/dashboard/dashboard.component.ts
+++ ClientApp/src/app/components/dashboard/dashboard.component.ts
@@ -1,28 +1,67 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { JiraService } from 'src/app/services/jira.service';
+import { transition, animate, trigger, state, style } from '@angular/animations';
+
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
+
 export class DashboardComponent implements OnInit {
-  public cards: Card[];
+  cards: Card[];
   squad: string;
+  authenticated: boolean = false;
+  fullName: string;
+  observations: string;
+
+  epicsSource = new MatTableDataSource<Card>();
+  displayedEpicsColumns: string[] = ['key', 'name', 'status', 'leadtime'];
+
+  issuesSource = new MatTableDataSource<Card>();
+  displayedIssuesColumns: string[] = ['parent', 'key', 'issuetype', 'summary', 'leadtime', 'bcpXhours', 'assignee', 'status'];
 
   constructor(private jiraService: JiraService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(params => {
       this.squad = params.get('squad');
+      this.fullName = params.get('name');
+
+      if(this.squad!=null && this.fullName!=null)
+        this.authenticated = true;
+      else
+        this.authenticated = false;
 
       this.jiraService.getCards(this.squad)
         .subscribe(result => {
           this.cards = result;
+
+          this.epicsSource = new MatTableDataSource(this.cards.filter(card => card.issuetype === 'Epic'));
+          this.issuesSource = new MatTableDataSource(this.cards.filter(card => card.issuetype !== 'Epic'));
         });
     });
   }
+
+  getObservations(key: string): string {
+    return localStorage.getItem(key);
+  }
+
+  saveLocalStorage(key: string){
+    localStorage.setItem(key, this.observations);
+    this.observations = "";
+  }
+
 }
 
 interface Card {
