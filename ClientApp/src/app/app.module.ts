3641c12f3c5f00b4fba3a4cb544e7061390048ba
Carlos Roberto Faria da Silva <ocarlosroberto@gmail.com>
2022-02-01 09:43:15 -0300

    feat: Install Material

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { LoginComponent } from './components/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+import { MatCardModule, MatIconModule, MatInputModule, MatTableModule, MatTabsModule, MatFormFieldModule, MatExpansionModule } from '@angular/material';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -20,23 +22,31 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    LoginComponent,
     DashboardComponent,
-
+    LoginComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
+
+    MatTableModule,
+    MatTabsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatExpansionModule,
+    MatIconModule,
+
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'fetch-data/:squad', component: FetchDataComponent },
-      { path: 'dashboard/:squad', component: DashboardComponent },
       { path: 'login', component: LoginComponent },
-    ])
+      { path: 'dashboard/:squad/:name', component: DashboardComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
